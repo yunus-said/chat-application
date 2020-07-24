f83e18709ab1a9de7a73dd094e21605693c0a481
Yunus <yunus@vt.edu>
2020-07-23 21:10:36 -0400

    Asynchronous Chat App v1.0

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Web Application for online chatting called PicChat.
---
+++ picChat/routing.py
@@ -0,0 +1,13 @@
+# picChat/routing.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chatApp.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chatApp.routing.websocket_urlpatterns
+        )
+    ),
+})
